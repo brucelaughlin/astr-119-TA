5455d961c348e995ab5639dc839bb8758fce9998
brucelaughlin <brucelaughlin@gmail.com>
2021-10-05 12:57:15 -0700

    Add files via upload

---
+++ shortcuts1.py
@@ -0,0 +1,56 @@
+# Useful shortcuts (towards using your mouse less and being faster!)
+#
+# WRITTEN WITH MAC OS IN MIND; SEE NOTES BELOW FOR WINDOWS
+# ----------------------------------------------------------------------
+# note: i'm abbreviating "command" with "cmd" for mac keyboards
+#
+# note: on windows, try swapping the "ctrl" key for the "cmd" key, and "alt" with "option"
+# if this doesn't work, some experimenting should help.  i can help too)
+# 
+# for more info, can search online for "sublime text shortcuts" etc., will find pages such as: 
+# https://itechbrand.com/the-sublime-text-3-keyboard-shortcuts-cheat-sheet/
+# https://shortcutworld.com/Sublime-Text/win/Sublime-Text_Shortcuts
+#
+# ----------------------------------------------------------------------
+# 
+# (cmd + z) undoes your last action.  this can be used to step back multiple steps
+#
+# holding down the (shift) key and moving the cursor will select everything you cover
+#
+# (cmd + c) "copy" and (cmd + v) "paste" work just like in a text editor
+#
+# with nothing selected, (cmd + c) copies the current line 
+# "                   ", (cmd + x) cuts the current line (you can paste it)
+# 
+# (control + shift + k)  deletes the current line.  the line can't be pasted
+# (control + k) deletes everything after the cursor on a line
+#
+# THIS IS SUPER USEFUL FOR CREATING REPETIVE BLOCKS OF CODE!!!
+## for instance, just put your cursor on some random line of code (using your mouse or
+## arrow keys).  with nothing selected, press (cmd + c).  now paste (cmd + v).  the copied
+## line should appear on a new line below the cursor.  you can move your cursor around
+## to paste the line wherever you like
+#
+# (cmd + left/right) moves the cursor to the beginning/end of line
+# (option + left/right) moves the curser by one word to the left/right
+# NOTE: CHECK THESE IF YOU'RE ON WINDOWS!  It may be (alt + left/right) to move to
+# the beginning/end of line, etc
+#
+# (cmd + enter) starts a blank new line below the cursor, regardless of where the cursor is
+#
+# holding down the (shift) key will select everything that cursor subsequently moves over.
+# selections can be copied, cut, deleted, etc
+#
+# (control + g) lets you enter a line number to jump to
+#
+# (cmd + ]) indents the current line
+# (cmd + ]) unindents the current line
+# note: this can be used to (un)indent multiple selected lines of code
+
+
+
+
+
+
+
+
